224449ece0bfedfff4576f1c2d7220084ccaa862
cecepasinechka <cvetankapasinecka@gmail.com>
2024-01-08 19:06:21 +0100

    EMB-119 sending messages using MQTT to node.js server which adds to database

--- app/src/main/java/com/example/eventfulmb/module/Message.kt
+++ app/src/main/java/com/example/eventfulmb/module/Message.kt
@@ -1,11 +1,12 @@
 package com.example.eventfulmb.module
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class Message(
     var body: String = "",
     var category: String = "",
-    var time: LocalDateTime = LocalDateTime.now(),
+    var time: String = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
     var latitude: Double = 0.0,
     var longitude: Double = 0.0
 ) {
